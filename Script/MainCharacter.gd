aae6dac298f8f1175bb014198d2c498227a0ce67
Abolfazl <Abolfazl0Delavary@gmail.com>
2025-01-11 23:34:06 +0330

    11 Jan

--- Script/MainCharacter.gd
+++ Script/MainCharacter.gd
@@ -16,8 +16,8 @@ func _physics_process(delta):
 	
 	if is_on_floor():
 		velocity.y = 0
-	
-	
+		Jump_permission = true
+
 	if Input.is_action_pressed("MoveLeft"):
 		velocity.x = -Speed
 	
@@ -27,16 +27,11 @@ func _physics_process(delta):
 	else: 
 		velocity.x = 0 
 	
-	if is_on_floor():
-		Jump_permission = true
-	else: 
-		Jump_permission = false
-	
-	if Input.is_action_just_pressed("Jump") and Jump_permission == true:
+	if Input.is_action_just_pressed("Jump") and Jump_permission:
 		velocity.y = Jump
 	
-	if Input.is_action_just_released("Jump") and Jump_permission == false and velocity.y < 0:
-		velocity.y = 0
+	if Input.is_action_just_released("Jump") and !is_on_floor() and velocity.y < -60:
+		velocity.y = 5
 	
 	if !is_on_floor():
 		$AnimatedSprite2D.animation = "jump"
@@ -51,17 +46,31 @@ func _physics_process(delta):
 		$AnimatedSprite2D.flip_h = true
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false
+	
+	if velocity.y >= 0 and !is_on_floor_only():
+		Jump_Delay()
+		#print("Delay Jump_permission = false")
+	elif !is_on_floor():
+		#print("Jump_Permission = false")
+		Jump_permission = false
+	
+
 
 	move_and_slide()
 
 func Gravity_process():
 	velocity.y += Gravity
 	
-	if velocity.y > 0:
+	if velocity.y > 0 :
 		await get_tree().create_timer(0.1).timeout
 		Gravity += 1
 	if Gravity > 11: 
 		Gravity = 11
-	if velocity.y > 170:
-		velocity.y = 170
+	if velocity.y > 180:
+		velocity.y = 180
 
+func Jump_Delay():
+	if velocity.y >= 0 and !is_on_floor_only():
+		await get_tree().create_timer(1).timeout
+		if velocity.y >= 0 and !is_on_floor_only():
+			Jump_permission = false
